0b14284b0996cd85cefd97f28c045fcd77d004ee
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-02-07 20:54:48 +0200

    progress update

--- app/src/main/java/com/okravi/trelli/activities/SignUpActivity.kt
+++ app/src/main/java/com/okravi/trelli/activities/SignUpActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.view.WindowManager
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.okravi.trelli.R
 import com.okravi.trelli.databinding.ActivitySignUpBinding
 
@@ -71,7 +75,38 @@ class SignUpActivity : BaseActivity() {
         val password: String = binding?.etPassword?.text.toString().trim {it <= ' '}
         
         if(validateForm(name, email, password)){
-            Toast.makeText(this, "User can be registered", Toast.LENGTH_SHORT).show()
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+
+                    hideProgressDialog()
+
+                    // If the registration is successfully done
+                    if (task.isSuccessful) {
+
+                        // Firebase registered user
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+                        // Registered Email
+                        val registeredEmail = firebaseUser.email!!
+
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            "$name you have successfully registered with email id $registeredEmail.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        FirebaseAuth.getInstance().signOut()
+                        // Finish the Sign-Up Screen
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            task.exception!!.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
         }
     }
 
