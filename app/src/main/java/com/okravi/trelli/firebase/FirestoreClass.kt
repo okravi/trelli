9c5134a4f0555d9e3dcc984378bba1591ad90e99
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-02-09 20:52:29 +0200

    progress update

---
+++ app/src/main/java/com/okravi/trelli/firebase/FirestoreClass.kt
@@ -0,0 +1,14 @@
+package com.okravi.trelli.firebase
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.okravi.trelli.activities.SignUpActivity
+import com.okravi.trelli.models.User
+
+class FirestoreClass {
+
+    private val mFirestore = FirebaseFirestore.getInstance()
+
+    fun registerUSer(activity: SignUpActivity, userInfo: User){
+
+    }
+}
